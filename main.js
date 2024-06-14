c8f62cbc0b2abc6b80b5d15eac6564581c174df1
Isteiar <isteiarahmed8@gmail.com>
2024-06-15 03:04:25 +0600

    create a object constructor for storing contact info + hide the contact form  using cancel button +  trying to solve the form input portion

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
 $('document').ready(function () {
-  $('#add-contact-button').on('click', function (ev) {
+  $('#add-contact-button').on('click', function () {
     $('.add-contact-form-container').show();
   });
   $('#submit').on('click', function (event) {
     event.preventDefault();
+    let addContact = [];
     const name = $('#name').val();
     const surname = $('#surname').val();
-    const phone = $('#phoneNumber').val();
     const address = $('#address').val();
-    console.log(name, surname, phone, address);
+    const phone = $('#phoneNumber').val();
+    // if(name!='' && surname!='' && address!='' && phone!=''){
+    //   let obj = new ContactInfo(name, surname, address, phone);
+    //   addContact.push(obj);
+    // console.log(addContact);}
+    console.log(name, surname, address, phone);
+  });$('#submit').on('click', function () {
+    $('.add-contact-form-container').hide();
   });
+  $('#reset').on('click',function(){
+    $('.add-contact-form-container').hide();
+  })
 });
+// $('#submit').on('click',function(){
+
+    
+//   }
+// })
+// function ContactInfo(name,surname,address,phone){
+//   this.name = name;
+//   this.surname = surname;
+//   this.address = address;
+//   this.phone = phone;
+
+// }
