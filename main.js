be0c55fa66f2e958dd6301b63189146ea9f58a1a
Isteiar Ahmed <isteiarahmed8@gmail.com>
2024-06-15 11:39:07 +0600

    update the js file

--- main.js
+++ main.js
@@ -1,35 +1,46 @@
+let contactList = [];
 $('document').ready(function () {
   $('#add-contact-button').on('click', function () {
     $('.add-contact-form-container').show();
   });
   $('#submit').on('click', function (event) {
     event.preventDefault();
-    let addContact = [];
     const name = $('#name').val();
     const surname = $('#surname').val();
     const address = $('#address').val();
     const phone = $('#phoneNumber').val();
-    // if(name!='' && surname!='' && address!='' && phone!=''){
-    //   let obj = new ContactInfo(name, surname, address, phone);
-    //   addContact.push(obj);
-    // console.log(addContact);}
-    console.log(name, surname, address, phone);
-  });$('#submit').on('click', function () {
+    if (name != '' && surname != '' && address != '' && phone != '') {
+      contactList.push({
+        name,
+        surname,
+        address,
+        phone,
+      });
+      $('.add-contact-form-container').hide();
+      $('#name').val('');
+      $('#surname').val('');
+      $('#address').val('');
+      $('#phoneNumber').val('');
+      const allContacts = contactList
+        .map(function (el, idx) {
+          return `<div class="contact">
+        <div class="information">
+          <div>Name: ${el.name}</div>
+          <div>Surname: ${el.surname}</div>
+          <div>Address: ${el.address}</div>
+          <div>Phone: ${el.phone}</div>
+        </div>
+        <button type="button" onclick="deleteContact(${idx})">Delete</button>
+        </div>`;
+        })
+        .join('\n');
+      $('.contactList').html(allContacts);
+    }
+  });
+
+  $('#reset').on('click', function () {
     $('.add-contact-form-container').hide();
   });
-  $('#reset').on('click',function(){
-    $('.add-contact-form-container').hide();
-  })
 });
-// $('#submit').on('click',function(){
 
-    
-//   }
-// })
-// function ContactInfo(name,surname,address,phone){
-//   this.name = name;
-//   this.surname = surname;
-//   this.address = address;
-//   this.phone = phone;
-
-// }
+function deleteContact(id) {}
